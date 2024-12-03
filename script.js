313e796dbbc8935757a5df9b8d825b9000eb70be
chefTee <taiwosharon36@gmail.com>
2024-12-03 16:47:46 +0100

    fixing mobile screen active state

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ const toolTip = document.querySelector('.tooltip');
 const mobileTooltip = document.querySelector('.tooltip-mobile');
 const profileSection = document.querySelector('.profile-container');
 
-// shareButton.addEventListener('mouseenter', () => {
-//     if(window.matchMedia('(min-width: 768px)').matches){
-//         toolTip.classList.toggle('hide');
-//     }else{
-//         profileSection.classList.toggle('hide');
-//         mobileTooltip.classList.toggle('hide');
-//     }
+shareButton.addEventListener('mouseenter', () => {
+    if(window.matchMedia('(min-width: 768px)').matches){
+        toolTip.classList.toggle('hide');
+    }else{
+        profileSection.classList.toggle('hide');
+        mobileTooltip.classList.toggle('hide');
+    }
     
-// })
+})
 
-// shareButton.addEventListener('mouseleave', () => {
-//     if(window.matchMedia('(min-width: 768px)').matches){
-//         toolTip.classList.toggle('hide');
-//     }else{
-//         mobileTooltip.classList.toggle('hide');
-//         profileSection.classList.remove('hide');
-//     }
+shareButton.addEventListener('mouseleave', () => {
+    if(window.matchMedia('(min-width: 768px)').matches){
+        toolTip.classList.toggle('hide');
+    }else{
+        mobileTooltip.classList.toggle('hide');
+        profileSection.classList.remove('hide');
+    }
 
-// })
+})
 
 shareButton.addEventListener('click', (e) => {
     if(window.matchMedia('(max-width: 767px)').matches){

ae76467fc40999fce3b6e434b8d89590aba94fe4
chefTee <taiwosharon36@gmail.com>
2024-12-03 16:53:04 +0100

    fixing mobile active state

--- script.js
+++ script.js
@@ -6,39 +6,40 @@ const profileSection = document.querySelector('.profile-container');
 shareButton.addEventListener('mouseenter', () => {
     if(window.matchMedia('(min-width: 768px)').matches){
         toolTip.classList.toggle('hide');
-    }else{
-        profileSection.classList.toggle('hide');
-        mobileTooltip.classList.toggle('hide');
     }
+    // }else{
+    //     profileSection.classList.toggle('hide');
+    //     mobileTooltip.classList.toggle('hide');
+    // }
     
 })
 
 shareButton.addEventListener('mouseleave', () => {
     if(window.matchMedia('(min-width: 768px)').matches){
         toolTip.classList.toggle('hide');
-    }else{
-        mobileTooltip.classList.toggle('hide');
-        profileSection.classList.remove('hide');
     }
+    // else{
+    //     mobileTooltip.classList.toggle('hide');
+    //     profileSection.classList.remove('hide');
+    // }
 
 })
 
-shareButton.addEventListener('click', (e) => {
+shareButton.addEventListener('click', () => {
     if(window.matchMedia('(max-width: 767px)').matches){
-        profileSection.classList.add('hide');
+        profileSection.classList.toggle('hide');
         mobileTooltip.classList.toggle('hide');
-        e.stopPropagation();
     }
 })
 
 
-mobileTooltip.addEventListener('click', (e) => {
-    e.stopPropagation();
-})
+// mobileTooltip.addEventListener('click', (e) => {
+//     e.stopPropagation();
+// })
 
-document.addEventListener('click', (event) => {
-    if(!mobileTooltip.contains(event.target)){
-        profileSection.classList.remove('hide');
-        mobileTooltip.classList.add('hide');
-    }
-})
+// document.addEventListener('click', (event) => {
+//     if(!mobileTooltip.contains(event.target)){
+//         profileSection.classList.remove('hide');
+//         mobileTooltip.classList.add('hide');
+//     }
+// })

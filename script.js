2cc94899f8927512f042318374f9b63d1e22dbdc
chefTee <taiwosharon36@gmail.com>
2024-12-03 13:52:44 +0100

    second commit

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const shareButton = document.querySelector('.share-container');
 const toolTip = document.querySelector('.tooltip');
+const mobileTooltip = document.querySelector('.tooltip-mobile');
+const profileSection = document.querySelector('.profile-container');
 
 shareButton.addEventListener('mouseenter', () => {
-    toolTip.classList.toggle('hide');
+    if(window.matchMedia('(min-width: 768px)').matches){
+        toolTip.classList.toggle('hide');
+    }else{
+        profileSection.classList.toggle('hide');
+        mobileTooltip.classList.toggle('hide');
+    }
+    
 })
 
 shareButton.addEventListener('mouseleave', () => {
-    toolTip.classList.toggle('hide');
+    if(window.matchMedia('(min-width: 768px)').matches){
+        toolTip.classList.toggle('hide');
+    }else{
+        mobileTooltip.classList.toggle('hide');
+        profileSection.classList.remove('hide');
+    }
+
 })
